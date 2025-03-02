e09e6d931efeca8dafa066b3f37c9bf7ed7437f8
Daniel Tolbert <danielchristopher19@gmail.com>
2025-03-02 15:50:09 -0500

    Added link to mysql database

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,13 +2,13 @@ import { createClient } from "@supabase/supabase-js";
 
 require('dotenv').config();
 
-const supabaseUrl: string = process.env.SUPABASE_URL || '';
-const supabaseKey: string = process.env.SUPABASE_KEY || '';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error('SUPABASE_URL and SUPABASE_KEY must be set');
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
--- backend/src/schema/002_seedUsers.sql
+++ backend/src/schema/002_seedUsers.sql
@@ -1,5 +1,8 @@
-INSERT INTO users (first_name, last_name, email, password) 
-VALUES ('Admin', 'User', 'admin@spiderchip.com', '$2b$10$9PjhDrW/Brc0dm9cd1kTSOzhwJGpDk2s/Qkg8.64aSbXAgdvB4bWe'); -- Admin123
+-- Seed Users according to the schema
 
-INSERT INTO users (first_name, last_name, email, password) 
-VALUES ('Spider', 'Chip', 'user@spiderchip.com', '$2b$10$4wtJliuSm2ZVqm89Ed6lE.2CIGziElxCPyTgZXaWJyoRLL5481gea'); -- Spiderchipuser123
+INSERT INTO users (username, email, password)
+VALUES ('admin', 'admin@spiderchip.com', '$2b$10$9PjhDrW/Brc0dm9cd1kTSOzhwJGpDk2s/Qkg8.64aSbXAgdvB4bWe'); -- Admin123
+
+INSERT INTO users (username, email, password)
+VALUES ('user', 'user@spiderchip.com', '$2b$10$4wtJliuSm2ZVqm89Ed6lE.2CIGziElxCPyTgZXaWJyoRLL5481gea'); -- Spiderchipuser123
+
