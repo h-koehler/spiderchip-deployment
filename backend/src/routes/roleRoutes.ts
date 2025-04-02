26ded941f6346e3c7040d7bc6db051f4b4e6e5a1
Shreyas <pauliteshreyas@gmail.com>
2025-04-02 15:42:51 -0700

    added tests for `Role` route

--- backend/src/routes/roleRoutes.ts
+++ backend/src/routes/roleRoutes.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { getRoles, getRole, createRole, updateRole, deleteRole, getRoleByName } from "../controllers";
 import { createRoleSchema, updateRoleSchema } from "../validation/roleSchema";
-import { validateRequest, authenticateJWT } from "../middleware";
+import { validateRequest, authenticateJWT, authorizeRoles } from "../middleware";
+import { Role } from "../config/roles";
 
 const router = express.Router();
 
-router.get("/", getRoles);
-router.get("/:id", getRole);
-router.get("/name/:name", getRoleByName);
-router.post("/", validateRequest(createRoleSchema), createRole);
-router.patch("/:id", validateRequest(updateRoleSchema), updateRole);
-router.delete("/:id", deleteRole);
+router.get("/", authenticateJWT, authorizeRoles(Role.ADMIN), getRoles);
+router.get("/:id", authenticateJWT, authorizeRoles(Role.ADMIN), getRole);
+router.get("/name/:name", authenticateJWT, authorizeRoles(Role.ADMIN), getRoleByName);
+router.post("/", authenticateJWT, authorizeRoles(Role.ADMIN), validateRequest(createRoleSchema), createRole);
+router.patch("/:id", authenticateJWT, authorizeRoles(Role.ADMIN), validateRequest(updateRoleSchema), updateRole);
+router.delete("/:id", authenticateJWT, authorizeRoles(Role.ADMIN), deleteRole);
 
 export default router;
