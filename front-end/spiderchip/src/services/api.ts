2b4d88580cfb54d75a432478fe9a7349bae9ed40
Shreyas <pauliteshreyas@gmail.com>
2025-04-27 12:19:21 -0700

    aws config changes

--- front-end/spiderchip/src/services/api.ts
+++ front-end/spiderchip/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const TOKEN_STORAGE_KEY = "auth-user-token";
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: '/api'
 });
 
 let userId: string | null = null;
