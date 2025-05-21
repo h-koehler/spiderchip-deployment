cc859ea13ed546dbbf3341a6dd18354a5319ef67
h-koehler <hannahkoehler19@gmail.com>
2025-05-21 16:10:46 -0400

    Changed compareLevelItems to support Chrome

--- front-end/spiderchip/src/types.ts
+++ front-end/spiderchip/src/types.ts
@@ -34,22 +34,41 @@ export function getUniqueLevelItemKey(a: LevelItem): string {
     return a.type + "_" + a.id
 }
 
+// export function compareLevelItems(a: LevelItem, b: LevelItem): number {
+//     if (a.type === LevelItemType.PUZZLE && b.type === LevelItemType.PUZZLE) {
+//         // puzzles just get ordered by the puzzle number
+//         return a.id - b.id;
+//     } else if (a.type === LevelItemType.PUZZLE && b.type === LevelItemType.STORY) {
+//         // stories queue up behind the associated puzzle
+//         return a.id <= b.level ? -1 : 1;
+//     } else if (a.type === LevelItemType.STORY && b.type === LevelItemType.PUZZLE) {
+//         // ditto as above
+//         return a.level > b.id ? -1 : 1;
+//     } else if (a.type === LevelItemType.STORY && b.type === LevelItemType.STORY) {
+//         // stories just get ordered by the story order
+//         return a.id - b.id;
+//     } else {
+//         throw new Error("Unknown comparison when comparing level items");
+//     }
+// }
+
+// chrome is dumb
 export function compareLevelItems(a: LevelItem, b: LevelItem): number {
-    if (a.type === LevelItemType.PUZZLE && b.type === LevelItemType.PUZZLE) {
-        // puzzles just get ordered by the puzzle number
-        return a.id - b.id;
-    } else if (a.type === LevelItemType.PUZZLE && b.type === LevelItemType.STORY) {
-        // stories queue up behind the associated puzzle
-        return a.id <= b.level ? -1 : 1;
-    } else if (a.type === LevelItemType.STORY && b.type === LevelItemType.PUZZLE) {
-        // ditto as above
-        return a.level > b.id ? -1 : 1;
-    } else if (a.type === LevelItemType.STORY && b.type === LevelItemType.STORY) {
-        // stories just get ordered by the story order
-        return a.id - b.id;
-    } else {
-        throw new Error("Unknown comparison when comparing level items");
+    const aBucket = a.type === LevelItemType.PUZZLE ? a.id : a.level;
+    const bBucket = b.type === LevelItemType.PUZZLE ? b.id : b.level;
+
+    // levels/ids do not match
+    if (aBucket !== bBucket) {
+        return aBucket - bBucket;
     }
+
+    // values do match -> comparing a story and a puzzle
+    if (a.type !== b.type) {
+        return a.type === LevelItemType.PUZZLE ? -1 : 1;
+    }
+
+    // comparing two stories with the same level
+    return a.id - b.id;
 }
 
 export enum LineHighlightType {
