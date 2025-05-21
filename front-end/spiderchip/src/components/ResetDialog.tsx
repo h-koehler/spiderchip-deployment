0c6bfff8ebe5ebcdf8412fd957024e0d0acd3a0d
h-koehler <hannahkoehler19@gmail.com>
2025-05-21 14:44:32 -0400

    Changed buttons in ResetDIalog

--- front-end/spiderchip/src/components/ResetDialog.tsx
+++ front-end/spiderchip/src/components/ResetDialog.tsx
@@ -17,8 +17,8 @@ export default function ResetDialog( props: {
                     <p>Reseting your level progress is permanent.</p>
                     <p>Are you sure you want to reset your progress?</p>
                     <div className="button-row">
-                        <button className="primary-button" onClick={props.onYes}>YES</button>
-                        <button className="primary-button" onClick={props.onNo}>NO</button>
+                        <button className="primary-button" onClick={props.onYes}>Yes, reset my progress</button>
+                        <button className="primary-button" onClick={props.onNo}>No</button>
                     </div>
                 </div>
             </div>
